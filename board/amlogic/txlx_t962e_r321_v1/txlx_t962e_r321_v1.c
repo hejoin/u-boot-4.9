895fdbb12891f1189c415656e8b3d885bfc95c16
Jian Hu <jian.hu@amlogic.com>
2018-05-17 15:03:41 +0800

    dm: i2c: add axg/g12a i2c platform data [1/1]
    PD#166544
    
    Problem:
    axg/g12a dm i2c not work
    
    Solution:
    Add axg/g12a i2c platform data
    
    Verify:
    axg_s400_v3
    g12a_u211_v1
    
    Change-Id: I9d9a621f44b342d9b3c981428528a43f4ffa2855
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>
    
    Conflicts:
    	board/amlogic/txlx_t962e_r321_v1/txlx_t962e_r321_v1.c

--- board/amlogic/txlx_t962e_r321_v1/txlx_t962e_r321_v1.c
+++ board/amlogic/txlx_t962e_r321_v1/txlx_t962e_r321_v1.c
@@ -345,6 +345,36 @@ U_BOOT_DEVICE(spicc0) = {
 };
 #endif /* CONFIG_AML_SPICC */
 
+#ifdef CONFIG_SYS_I2C_MESON
+static const struct meson_i2c_platdata i2c_data[] = {
+	{ 0, 0xffd1f000, 166666666, 3, 15, 100000 },
+	{ 1, 0xffd1e000, 166666666, 3, 15, 100000 },
+	{ 2, 0xffd1d000, 166666666, 3, 15, 100000 },
+	{ 3, 0xffd1c000, 166666666, 3, 15, 100000 },
+	{ 4, 0xff805000, 166666666, 3, 15, 100000 },
+};
+
+U_BOOT_DEVICES(meson_i2cs) = {
+	{ "i2c_meson", &i2c_data[0] },
+	{ "i2c_meson", &i2c_data[1] },
+	{ "i2c_meson", &i2c_data[2] },
+	{ "i2c_meson", &i2c_data[3] },
+	{ "i2c_meson", &i2c_data[4] },
+};
+
+/*
+ *GPIODV_0//I2C_SDA_B
+ *GPIODV_1//I2C_SCK_B
+ *pinmux configuration seperated with i2c controller configuration
+ * config it when you use
+ */
+void set_i2c1_pinmux(void)
+{
+	clrbits_le32(P_PERIPHS_PIN_MUX_9, 1 << 16 | 1 << 17);
+	clrbits_le32(P_PERIPHS_PIN_MUX_2, 1 << 31);
+	setbits_le32(P_PERIPHS_PIN_MUX_2, 1 << 24 | 1 << 25);
+}
+#endif /*end CONFIG_SYS_I2C_MESON*/
 
 extern void aml_pwm_cal_init(int mode);
 
